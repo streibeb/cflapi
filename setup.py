4e0bf609bab94f0f8c7f623409de7fa16bad2f78
Brayden Streibel <brayden@streibel.ca>
2018-10-26 20:25:29 -0600

    chore(Setup): Published to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
+import sys, os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
 setuptools.setup(
     name="cflapi",
-    version="1.0.0",
+    version="1.0.2",
     author="Brayden Streibel",
     author_email="brayden@streibel.ca",
     description="Canadian Football League API Wrapper",
@@ -19,4 +26,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=['requests'],
+    license='MIT',
 )
